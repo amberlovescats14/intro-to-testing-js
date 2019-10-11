c291b27fd325e7dcb1363be70caf1e2ce3cc8ce8
Amber Jones <amberlovescats14@gmail.com>
2019-10-11 14:53:57 -0500

    first test

--- code.js
+++ code.js
@@ -1,4 +1,36 @@
-// helloWorld function 
+//*_____ NO TEST >
+//@ if input is a number ...truthy
+const isNumeric = value => value == parseFloat(value)
+//     !isNan(parseFloat(value))
+
+//@ is less than 0
+const isNegative = num => parseFloat(num) < 0
+
+//@ greater than 0
+const isPositive = input => parseFloat(input) > 0
+
+//@ input is non-numeric //@ I wrote that funny
+const isNotNumeric = input => !isNumeric(input)
+
+//*_______ < END NO TEST
+
+//!!! TESTED FUNCTIONS FROM HERE DOWN !!////
+
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
 }
+
+const valueIsTrue = val => val === true;
+
+//@ is an array or not
+const isArray = input => Array.isArray(input);
+
+//@ has to be number and has to be zero
+const isZero = num => (
+    !isNegative(num) && !isPositive(num) && isNumeric(num)
+)
+
+//@ say hello name
+const sayHello = name => `hello ${amber}`
+
--- tests.js
+++ tests.js
@@ -13,3 +13,73 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+// writing a second unit test
+describe('valueIsTrue',
+    function () {
+        it(`should be a defined function`, function(){
+            expect(typeof valueIsTrue).toBe('function')
+        });
+        it('should return a boolean', function () {
+            expect(typeof valueIsTrue()).toBe('boolean')
+        });
+        it('should return true', function () {
+            let value = true;
+           expect(valueIsTrue(value)).toBe(true)
+            
+        })
+    })
+
+// is array
+describe('isArray', ()=> {
+    it(`should be a defined function`, ()=> {
+        expect(typeof isArray).toBe('function')
+    });
+    it('should return a boolean', ()=> {
+        expect(typeof isArray()).toBe('boolean')
+    });
+    it('should should return true when passed an empty arr', () => {
+        let arr = []
+        expect(isArray(arr)).toBe(true)
+    });
+    it('should should be false when passed object', () => {
+        let obj = {}
+        expect(isArray(obj)).toBe(false)
+    })
+})
+
+
+//isZero
+describe('isZero', () => {
+    it(`should be a defined function`, () => {
+        expect(typeof isZero).toBe('function')
+    });
+    it('should should be a number', function () {
+        expect(typeof isZero()).toBe('boolean')
+    });
+    it('should return true if passed 0', function () {
+        let number = 0;
+        expect(isZero(number)).toBe(true)
+    });
+    it('should return false', function () {
+        expect(isZero(-1)).toBe(false)
+        expect(isZero(1)).toBe(false)
+    })
+})
+
+//@ sayHello
+describe(`sayHello`, ()=> {
+    it(`should be defined as a function`, ()=> {
+        expect(typeof sayHello).toBe('function')
+    });
+    it(`should return a string`, ()=> {
+        expect(typeof sayHello()).toBe('string')
+    });
+    it(`should return hello amber`, ()=> {
+        let name = 'amber'
+        expect(sayHello(name)).toBe('hello amber')
+    });
+    it(`should never be undefined`, ()=> {
+        expect(sayHello()).not.toBe(undefined)
+    })
+})
